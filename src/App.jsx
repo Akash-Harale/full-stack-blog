12a3a7f9ce4993e5401c5ba6d33823c76945c193
Divya Patil <patil.p.divya@gmail.com>
2025-04-08 15:59:23 +0530

    30 min completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-const App = () => {
-  return (
-    <div className=''>Hello World</div>
-  )
-}
+import Navbar from "./components/Navbar";
 
-export default App
+const App = () => {
+  return <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64" >      {/* Navbar */}
+  <Navbar />
+  {/* Breadcrumb */}
+  {/* Introduction */}
+  {/* Featured post */}
+  {/* Post list */} </div>;
+};
+
+export default App;
